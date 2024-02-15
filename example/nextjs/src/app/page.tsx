cc032ae81da4823e64f38ae96c11b4edf7e54f1b
강성운/디지털CRM팀 <woony@hyundai-autoever.com>
2024-02-15 17:39:32 +0900

    feat

--- example/nextjs/src/app/page.tsx
+++ example/nextjs/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Top } from "woony-design-system";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <Top />
     </main>
   );
